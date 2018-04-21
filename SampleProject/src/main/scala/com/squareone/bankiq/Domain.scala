85d86d672c4c53fc40d8e19405ce7b75656a7fd0
utkarsh <utkarshd80@gmail.com>
2018-04-21 19:01:24 +0530

    Cleaning up the repo.

--- SampleProject/src/main/scala/com/squareone/bankiq/Domain.scala
+++
@@ -1,6 +0,0 @@
-package com.squareone.bankiq
-
-case class MIS(srNo: String,	payer: String, name: String,	product: String,	invoiceNo: String,	invoiceDate: String,
-               invoiceAmount: Double,	discountingDate: String, inCharge: String, region: String, rec: String, grossCollection: String,
-               balance: Double, collectionDate: String, dueDate: String, usance: Double,	discountingTenure: Double, rateDisc: Double,
-               earlyCollection: Double,	collectionIncentive: Double,	netAmount: Double)
